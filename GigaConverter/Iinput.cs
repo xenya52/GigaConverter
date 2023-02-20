9af240f7cb6d1035b10808a2d04d03a94f42cdba
Xennoxx <nicknifffe@gmail.com>
2023-02-20 21:58:19 +0100

    Projektdateien hinzufügen.

---
+++ GigaConverter/Iinput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GigaConverter
+{
+    internal class Iinput
+    {
+        public string i1 { get; set; }
+        public string i2 { get; set; }
+    }
+}
