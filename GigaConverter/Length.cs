c45f607eb8ae71929a81309b846d3ec126f59031
Xennoxx <nicknifffe@gmail.com>
2023-02-22 07:53:49 +0100

    Notes advanced

--- GigaConverter/Length.cs
+++ GigaConverter/Length.cs
@@ -32,6 +32,11 @@ namespace GigaConverter
     }
 }
 
-/* Meter (=> Kilometer /1000 => Zentimeter *100 => Milimeter *1000 => 1* 10 hoch 6 
-   Kilometer (=> Meter * 1000 => Zentimeter 100000 => 1* 10 hoch 6 => 1* 10 hoch 9
-*/
+/* 
+ * Kilometer  (=> Meter * 1000 => Zentimeter 100000 => Millimeter 1* 10 hoch 6 => Mikrometer 1* 10 hoch 9 => Nanometer 1* 10 hoch 12)
+ * Meter      (=> Kilometer /1000 => Zentimeter *100 => Millimeter *1000 => Mikrometer 1* 10 hoch 6 => Nanometer 1* 10 hoch 9)
+ * Zentimeter (=> Kilometer /100000 => Meter /100 => Millimeter *10 => Mikrometer *10000 => Nanometer 1* hoch 7)
+ * Millimeter (=> Kilometer 1* 10 hoch 6 => Meter *1000 => Zentimeter *10 => Mikrometer *1000 =>Nanometer => 1* 10 hoch 6)
+ * Mikrometer (=> Kilometer=> Meter=> Zentimeter=> Millimeter =>Nanometer
+ * Nanometer  (=> Kilometer=> Meter=> Zentimeter=> Millimeter =>Mikrometer
+ */
