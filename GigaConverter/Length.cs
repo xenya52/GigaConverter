f4eb9489edb745faa36c8cf6dc07ed254ad8137a
Xennoxx <nicknifffe@gmail.com>
2023-03-03 10:24:20 +0100

    "System.Action" error but better than before...

--- GigaConverter/Length.cs
+++ GigaConverter/Length.cs
@@ -12,22 +12,39 @@ namespace GigaConverter
         public int Input
         {
             get; set;
-        } 
+        }
         //Wert für die Umwandlung der Eingabe
-        public int[] lengths = new int [6] ;
+        public /*just 4 the test public*/ int[] Lengths { get; set; } = new int[5];
         //Umwandlung der Eingabe
         public void Convert()
         {
-
+            switch (Lengths[0])
+            {
+                case 1:
+                    Console.WriteLine("Km in Meter");
+                    break;
+                case 2:
+                    Console.WriteLine("Km in Zentimeter");
+                    break;
+                case 3:
+                    Console.WriteLine("Km in Millimeter");
+                    break;
+                case 4:
+                    Console.WriteLine("Km in Mikrometer");
+                    break;
+                case 5:
+                    Console.WriteLine("Km in Nanometer");
+                    break;
+            }
         }
     }
 }
 
 /* 
- * Kilometer  (=> Meter * 1000 => Zentimeter 100000 => Millimeter 1* 10 hoch 6 => Mikrometer 1* 10 hoch 9 => Nanometer 1* 10 hoch 12)
- * Meter      (=> Kilometer /1000 => Zentimeter *100 => Millimeter *1000 => Mikrometer 1* 10 hoch 6 => Nanometer 1* 10 hoch 9)
- * Zentimeter (=> Kilometer /100000 => Meter /100 => Millimeter *10 => Mikrometer *10000 => Nanometer 1* hoch 7)
- * Millimeter (=> Kilometer 1* 10 hoch 6 => Meter *1000 => Zentimeter *10 => Mikrometer *1000 =>Nanometer => 1* 10 hoch 6)
- * Mikrometer (=> Kilometer=> Meter=> Zentimeter=> Millimeter =>Nanometer
- * Nanometer  (=> Kilometer=> Meter=> Zentimeter=> Millimeter =>Mikrometer
+ * 0Kilometer  (=> Meter * 1000 => Zentimeter 100000 => Millimeter 1* 10 hoch 6 => Mikrometer 1* 10 hoch 9 => Nanometer 1* 10 hoch 12)
+ * 1Meter      (=> Kilometer /1000 => Zentimeter *100 => Millimeter *1000 => Mikrometer 1* 10 hoch 6 => Nanometer 1* 10 hoch 9)
+ * 2Zentimeter (=> Kilometer /100000 => Meter /100 => Millimeter *10 => Mikrometer *10000 => Nanometer 1* hoch 7)
+ * 3Millimeter (=> Kilometer 1* 10 hoch 6 => Meter *1000 => Zentimeter *10 => Mikrometer *1000 =>Nanometer => 1* 10 hoch 6)
+ * 4Mikrometer (=> Kilometer=> Meter=> Zentimeter=> Millimeter =>Nanometer
+ * 5Nanometer  (=> Kilometer=> Meter=> Zentimeter=> Millimeter =>Mikrometer
  */
--- GigaConverter/Program.cs
+++ GigaConverter/Program.cs
@@ -1,7 +1,13 @@
-﻿class Program
-{
-    public static void main(string[] args)
-    {
+﻿using GigaConverter;
 
+class program
+{
+    public static void Main(string[] args)
+    {
+        //test
+        Length test1 = new();
+        test1.Lengths[0] = 1;
+        Console.WriteLine(test1.Convert);
+        //failed offf...
     }
 }
